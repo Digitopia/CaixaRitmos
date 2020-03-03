ff17f73a747d991d34156a335afaa5f05423b4c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-03 12:07:09 +0000

    Deploy Digitopia/CaixaRitmos to github.com/Digitopia/CaixaRitmos.git:gh-pages

---
+++ precache-manifest.c9a772b2e38dc2a978518f0140ec0ede.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141ddae5215eb2f168a9",
+    "url": "/CaixaRitmos/css/app.fe82df32.css"
+  },
+  {
+    "revision": "0b72d600661e25266c873d980e1a66dc",
+    "url": "/CaixaRitmos/fittext.js"
+  },
+  {
+    "revision": "e4100af8caf67353ad848e5d9e975cbe",
+    "url": "/CaixaRitmos/icon.jpeg"
+  },
+  {
+    "revision": "86ea5062b742367ff2fec59ba9107a82",
+    "url": "/CaixaRitmos/img/agogo.png"
+  },
+  {
+    "revision": "560ba7235f9473e320b535f7b2f182aa",
+    "url": "/CaixaRitmos/img/bongos.png"
+  },
+  {
+    "revision": "513ee73fa4f5a0f215d1048c1aaac3c3",
+    "url": "/CaixaRitmos/img/clavas.png"
+  },
+  {
+    "revision": "ed82ceb5e633ee46f72d82236a3480bd",
+    "url": "/CaixaRitmos/img/congas.png"
+  },
+  {
+    "revision": "07b6f4470bb11bd4cdc56a2054fb653c",
+    "url": "/CaixaRitmos/img/darbuka.png"
+  },
+  {
+    "revision": "248d99898a422bbb9cedafd94b8f0020",
+    "url": "/CaixaRitmos/img/maracas.png"
+  },
+  {
+    "revision": "01b891276705c2ac48b80343a71e9397",
+    "url": "/CaixaRitmos/img/pandeireta.png"
+  },
+  {
+    "revision": "d26f541634d3d1fe9dadf63ed939d728",
+    "url": "/CaixaRitmos/img/triangulo.png"
+  },
+  {
+    "revision": "8762945a5e7cd4f0ab1f4267c9f04b04",
+    "url": "/CaixaRitmos/index.html"
+  },
+  {
+    "revision": "141ddae5215eb2f168a9",
+    "url": "/CaixaRitmos/js/app.68156f52.js"
+  },
+  {
+    "revision": "664abeb781918d0b0e80",
+    "url": "/CaixaRitmos/js/chunk-vendors.9f4bd590.js"
+  },
+  {
+    "revision": "3f65912ac4fdc62f42205f49dc12df8b",
+    "url": "/CaixaRitmos/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/CaixaRitmos/robots.txt"
+  },
+  {
+    "revision": "b351dfcdff04093f1a68fb8033fcc0fe",
+    "url": "/CaixaRitmos/sounds/agogo.mp3"
+  },
+  {
+    "revision": "37753ec1aa6c4fefda8777ca6d221d3f",
+    "url": "/CaixaRitmos/sounds/bongos.mp3"
+  },
+  {
+    "revision": "44c326e81d35183d663461b6f2c563e8",
+    "url": "/CaixaRitmos/sounds/clavas.mp3"
+  },
+  {
+    "revision": "e98cc8e9c877b198c680fce66c0553d7",
+    "url": "/CaixaRitmos/sounds/congas.mp3"
+  },
+  {
+    "revision": "d0e1b8d4e83c37d22fb028621d0d51ff",
+    "url": "/CaixaRitmos/sounds/darbuka.mp3"
+  },
+  {
+    "revision": "6caecece9b4d4a4358eadf596adea00e",
+    "url": "/CaixaRitmos/sounds/maracas.mp3"
+  },
+  {
+    "revision": "bc19af044985ad198e2d4d3358d7e913",
+    "url": "/CaixaRitmos/sounds/pandeireta.mp3"
+  },
+  {
+    "revision": "60247c0831629d4e68843848db2cc762",
+    "url": "/CaixaRitmos/sounds/triangulo.mp3"
+  }
+]);
